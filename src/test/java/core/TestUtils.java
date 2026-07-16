8b5428cbe0f05e1ee5d52cec550431c5cd090ffd
Your Name <lamriatampubolo@gmail.com>
2026-07-16 14:15:10 +0700

    Fix : File TestUtils

--- src/test/java/core/TestUtils.java
+++ src/test/java/core/TestUtils.java
@@ -1,27 +1,23 @@
 package core;
 
 import io.restassured.specification.RequestSpecification;
-
 import io.restassured.response.Response;
-
 import java.util.*;
-
 import static io.restassured.RestAssured.given;
-
 public class TestUtils extends BaseApiTest{
 
-    public static Response utilRequest(String endpointName, String graphqlQuery, Object variables, String username, String password, String sessionId) {
+    public static Response utilRequest(String graphqlQuery, Object variables, String username, String password, String sessionId) {
         String contentType = "application/json";
         Map<String, Object> requestBody = new HashMap<>();
         requestBody.put("query", graphqlQuery);
         if (variables != null) requestBody.put("variables", variables);
 
         RequestSpecification request = given().contentType(contentType);
-        // Basic Auth
+// Basic Auth
         if (username != null && !username.isBlank()) {
             request.auth().preemptive().basic(username, password);
         }
-        // Session Cookie
+// Session Cookie
         if (sessionId != null && !sessionId.isBlank()) {
             request.cookie("sid_b2b", sessionId);
         }
@@ -38,6 +34,34 @@ public class TestUtils extends BaseApiTest{
 
         return response;
     }
+    public static String getSid() {
+    final String mutation = "mutation Login($companyId: String!, $usernameOrEmail: String!, $password: String!) {\n" +
+                " login(companyId: $companyId, usernameOrEmail: $usernameOrEmail, password: $password) {\n" +
+                " user { id }\n" +
+                " errors { field message }\n" +
+                " }\n" +
+               " }";
+
+
+
+        Map<String, Object> variables = new HashMap<>();
+
+        variables.put("usernameOrEmail", config.getProperty("validEmailLogin"));
+
+        variables.put("password", config.getProperty("validPasswordLogin"));
+
+        variables.put("companyId", config.getProperty("companyId"));
+
+
+
+        Response response = utilRequest(mutation, variables, null, null, null);
+
+
+
+        return response.getCookie("sid_b2b");
+
+    }
+
 
 
 }
